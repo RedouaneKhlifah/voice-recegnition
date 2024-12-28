90893a19a48f6c348f3e0bd1cb36756242abf874
RedouaneKhlifahLf <redouane.khlifah@araba.shop>
2024-12-29 00:57:31 +0100

    delete some files

--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Test:React.FC = ()=> {
-  return (
-    <div className = "bg-red text-white">Test</div>
-  )
-}
-
-export default Test
--- src/utils/speechUtils.ts
+++
@@ -1,6 +0,0 @@
-export const speak = (text: string) => {
-    const speech = new SpeechSynthesisUtterance(text);
-    speech.lang = "en-US"; // Set language to English
-    window.speechSynthesis.speak(speech);
-  };
-  
